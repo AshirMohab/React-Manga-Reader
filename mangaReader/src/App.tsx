5518b1bccb6273bc59394bc891548666ca56b57a
AshirMohab <amohabir@jhb.dvt.co.za>
2022-03-02 08:09:32 +0200

    feat: :tada: Initial commit, set up tailwind and react-ts

---
+++ mangaReader/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500"> The blue sky</div>;
+}
+
+export default App;
