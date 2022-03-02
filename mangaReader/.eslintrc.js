3dd7c57e215492247f1726d76b0dada3cf025a6c
AshirMohab <amohabir@jhb.dvt.co.za>
2022-03-02 09:37:12 +0200

    feat: :tada: Eslint makes me angry, but config is done

---
+++ mangaReader/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [`plugin:react/recommended`, `airbnb`],
+  parser: `@typescript-eslint/parser`,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    ecmaVersion: `latest`,
+    sourceType: `module`,
+  },
+  plugins: [`react`, `@typescript-eslint`],
+  rules: {
+    quotes: [`error`, `backtick`],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: [`.js`, `.jsx`, `.ts`, `.tsx`] },
+    ],
+    'import/extensions': [`error`, `never`],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+};
--- mangaReader/src/App.tsx
+++ mangaReader/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <div className="bg-red-500"> The blue sky</div>;
 }
 
